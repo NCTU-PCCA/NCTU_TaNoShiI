14331973ab9cf6c1fd2ea59f6367474f785b9fba
hank55663 <joy09272002@gmail.com>
2018-10-04 22:48:42 +0800

    LCT dominator planar palindromic simplex

---
+++ codebook/code/String/palindromic_tree.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+#include<unistd.h>
+using namespace std;
+#define F first
+#define S second
+#define MP make_pair
+#define PB push_back
+#define IOS ios_base::sync_with_stdio(0); cin.tie(0);
+#define SZ(x) ((int)((x).size()))
+#define ALL(x) begin(x),end(x)
+#define REP(i,x) for (int i=0; i<(x); i++)
+#define REP1(i,a,b) for (int i=(a); i<=(b); i++)
+
+struct palindromic_tree{
+  struct node{
+    int next[26],fail,len;
+    int cnt,num,st,ed;
+    node(int l=0):fail(0),len(l),cnt(0),num(0){
+      for(int i=0;i<26;++i)next[i]=0;
+    }
+  };
+  vector<node> state;
+  vector<char> s;
+  int last,n;
+
+  void init(){
+    state.clear();
+    s.clear();
+    last=1;
+    n=0;
+    state.push_back(0);
+    state.push_back(-1);
+    state[0].fail=1;
+    s.push_back(-1);
+  }
+  int get_fail(int x){
+    while(s[n-state[x].len-1]!=s[n])x=state[x].fail;
+    return x;
+  }
+  void add(int c){
+    s.push_back(c-='a');
+    ++n;
+    int cur=get_fail(last);
+    if(!state[cur].next[c]){
+      int now=state.size();
+      state.push_back(state[cur].len+2);
+      state[now].fail=state[get_fail(state[cur].fail)].next[c];
+      state[cur].next[c]=now;
+      state[now].num=state[state[now].fail].num+1;
+    }
+    last=state[cur].next[c];
+    ++state[last].cnt;
+  }
+  int size(){
+    return state.size()-2;
+  }
+}pt;
+
+int main() {
+  string s;
+  cin >> s;
+  pt.init();
+  for (int i=0; i<SZ(s); i++) {
+    int prvsz = pt.size();
+    pt.add(s[i]);
+    if (prvsz != pt.size()) {
+      int r = i;
+      int l = r - pt.state[pt.last].len + 1;
+      cout << "Find pal @ [" << l << " " << r << "] : " << s.substr(l,r-l+1) << endl;
+    }
+  }
+
+  return 0;
+}
