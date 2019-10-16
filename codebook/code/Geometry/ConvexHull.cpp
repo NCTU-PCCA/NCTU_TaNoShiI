a18aab5b975a0862f8aaab81125b9b813d99b0e7
hank55663 <joy09272002@gmail.com>
2019-10-16 18:49:16 +0800

    short something

--- codebook/code/Geometry/ConvexHull.cpp
+++ codebook/code/Geometry/ConvexHull.cpp
@@ -1,13 +1,10 @@
 sort(p,p+n);
 pii ans[N];
 ans[0]=p[0];
-int k=0;
-int now=0;
-for(int yy=0;yy<2;yy++){
+int k=0,now=0;
+for(int tt=0;tt<2;tt++){
 	for(int i=1;i<n;i++){
-		while(now!=k&&cross(ans[now].x,p[j].x,ans[now-1].x)<0){
-			now--;
-		} 
+		while(now!=k&&cross(ans[now],p[j],ans[now-1])<0) now--;
 		ans[++now]=p[i];
 	}
 	k=now;
