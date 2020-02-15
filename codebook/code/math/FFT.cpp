36856d06a8ca2b4c6f0b4af894a00fe62508e39a
hank55663 <joy09272002@gmail.com>
2020-02-15 18:13:21 +0800

    fix format

--- codebook/code/math/FFT.cpp
+++ codebook/code/math/FFT.cpp
@@ -6,18 +6,15 @@ C a[N],b[N];
 void FFTinit(){
 	for (i=1;i<N;i++) g[i]=g[i>>1]>>1|((i&1)<<18);
 }
-void FFT(C *a,int f)
-{
+void FFT(C *a,int f){
 	int i,j,k,p;
 	for (i=0;i<N;i++)
 		if (g[i]>i) swap(a[i],a[g[i]]);
-	for (i=1;i<N;i<<=1)
-	{
+	for (i=1;i<N;i<<=1){
 		C e(cos(pi/i),f*sin(pi/i));
-		for (j=0;j<N;j+=i<<1)
-		{
-			C w(1,0);for (k=0;k<i;k++,w*=e)
-			{
+		for (j=0;j<N;j+=i<<1){
+			C w(1,0);
+			for (k=0;k<i;k++,w*=e){
 				C x=a[j+k],y=w*a[j+k+i];
 				a[j+k]=x+y;a[j+k+i]=x-y;
 			}
@@ -25,8 +22,7 @@ void FFT(C *a,int f)
 	}
 }
 int res[400005];
-int main()
-{
+int main(){
 	FFTinit();
 	FFT(a,1);
 	FFT(b,1);
