85870018ba904d8504dea79843897babbb56e43e
hank55663 <hank55663@DESKTOP-UF3EJBR.localdomain>
2020-03-11 18:29:12 +0800

    fix format of det

--- codebook/code/Other/det.cpp
+++ codebook/code/Other/det.cpp
@@ -1,17 +1,18 @@
-LL det(LL a[][20],int n) {  
-    LL ret=1;  
-    for(int i=1;i<n;i++){  
-        for(int j=i+1;j<n;j++)  
+LL det(LL a[][20],int n){  
+    LL ret = 1;  
+    for(int i = 1 ; i < n ; i++){  
+        for(int j = i + 1 ; j < n ; j++){
             while(a[j][i]){  
-                LL t=a[i][i]/a[j][i];  
-                for(int k=i;k<n;k++)  
-                    a[i][k]=a[i][k]-a[j][k]*t;  
-                for(int k=i;k<n;k++)  
-                    swap(a[i][k],a[j][k]);  
-                ret=-ret;  
+                LL t = a[i][i] / a[j][i];  
+                for(int k = i ; k < n ; k++)  
+                    a[i][k] = a[i][k] - a[j][k] * t;  
+                for(int k = i ; k < n ; k++)  
+                    swap(a[i][k], a[j][k]);  
+                ret = -ret;  
             }  
-        if(a[i][i]==0)return 0;  
-        ret=ret*a[i][i];  
+        }
+        if(a[i][i] == 0)return 0;  
+        ret = ret * a[i][i];    
     }  
     return ret;
 }  
