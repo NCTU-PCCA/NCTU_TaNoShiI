d10fe4348b7ed451112023e041ba648eeb74a602
hank55663 <joy09272002@gmail.com>
2020-03-26 00:03:54 +0800

    resize codebook, fix max clique, add KSP and CLCS

--- codebook/code/Other/dynamic_convex.cpp
+++ codebook/code/Other/dynamic_convex.cpp
@@ -1,69 +1,34 @@
-struct dynamic_convex{
-    static int op;
-    struct Line{
-        LL a,b;
-        LL x; 
-        bool operator<(const Line &q) const{
-            if(op == 0)return a < q.a;
-            else return x < q.x;
-        }
-    };
-    set<Line> s;
-    void add_Line(Line l){
-        op = 0;
-        auto it = s.lower_bound(l);
-        if(it != s.end() && it->a == l.a && it->b >= l.b)return;
-        else{
-            if(it != s.end() && it->a == l.a && it->b < l.b)
-                s.erase(it);
-            it = s.lower_bound(l);
-            if(it != s.begin() && it != s.end()){
-                auto it2 = prev(it);
-                LL x = (it2->b - l.b) / (l.a - it2->a), x2 = (l.b - it->b) / (it->a - l.a);
-                if(it2->b >= l.b)x++;
-                if(l.b >= it->b)x2++;
-                if(x >= x2)return;
-            }
-            while(it!=s.end()){
-            //   printf("!\n");
-                Line l1 = *it;
-                it = next(it);
-                s.erase(l1);
-                int x = (l.b-l1.b) / (l1.a-l.a);
-                if(l.b > l1.b)x++;
-                if(it != s.end() && it->x <= x)continue;
-                else{
-                    l1.x = x;
-                    s.insert(l1);
-                    break;
-                }
-            }
-            it = s.lower_bound(l);
-            LL x=-1e9-7;
-            while(it != s.begin()){
-                //printf("!\n");
-                auto it2 = prev(it);
-                x = (it2->b - l.b) / (l.a - it2->a);
-                if(it2->b >= l.b)x++;
-                if(it2->x >= x){
-                    s.erase(it2);
-                    x = -1e9-7;
-                }
-                else{
-                    break;
-                }
-            }
-            l.x = x;
-        //   printf("%d\n",x);
-            s.insert(l);
-        }
-    }
-    LL query(int p){
-        if(s.empty())return -2e18;
-        op = 1;
-        Line l;
-        l.x = p;
-        auto it = prev(s.upper_bound(l));
-        return it->a * p + it->b;
-    }
+struct LiChao_min{
+  struct line{
+    LL m, c;
+    line(LL _m=0, LL _c=0) { m = _m; c = _c; }
+    LL eval(LL x) { return m * x + c; }
+  };
+  struct node{
+    node *l, *r; line f;
+    node(line v) { f = v; l = r = NULL; }
+  };
+  typedef node* pnode;
+  pnode root; int sz;
+#define mid ((l+r)>>1)
+  void insert(line &v, int l, int r, pnode &nd){
+    if(!nd) { nd = new node(v); return; }
+    LL trl = nd->f.eval(l), trr = nd->f.eval(r);
+    LL vl = v.eval(l), vr = v.eval(r);
+    if(trl <= vl && trr <= vr) return;
+    if(trl > vl && trr > vr) { nd->f = v; return; }
+    if(trl > vl) swap(nd->f, v);
+    if(nd->f.eval(mid) < v.eval(mid)) insert(v, mid + 1, r, nd->r);
+    else swap(nd->f, v), insert(v, l, mid, nd->l);
+  }
+  LL query(int x, int l, int r, pnode &nd){
+    if(!nd) return LLONG_MAX;
+    if(l == r) return nd->f.eval(x);
+    if(mid >= x) return min(nd->f.eval(x), query(x, l, mid, nd->l));
+    return min(nd->f.eval(x), query(x, mid + 1, r, nd->r));
+  }
+  /* -sz <= query_x <= sz */
+  void init(int _sz){ sz = _sz + 1; root = NULL; }
+  void add_line(LL m, LL c){ line v(m, c); insert(v, -sz, sz, root); }
+  LL query(LL x) { return query(x, -sz, sz, root); }
 };
--- codebook/code/math/Theorem.cpp
+++ codebook/code/math/Theorem.cpp
@@ -9,4 +9,9 @@ A = i + b/2 - 1
 Erdős–Gallai theorem
 \sum_{i=1}^k d_i \leq k*(k-1)+\sum_{i=k+1}^n min(d_i,k)
 d_i decrease
+-------------------------------------------------------
+\item Stirling Numbers(permutation $|P|=n$ with $k$ cycles): \\
+  $S(n,k) = \text{coefficient of }x^k \text{ in } \Pi_{i=0}^{n-1} (x+i)$
+\item Stirling Numbers(Partition $n$ elements into $k$ non-empty set): \\
+  $S(n,k) = \frac{1}{k!} \sum\limits_{j=0}^k (-1)^{k-j} {k \choose j} j^n$
 */
