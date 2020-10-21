bd3e9586fa11270f9426b3c25514a0fbcc727f3d
hank55663 <joy09272002@gmail.com>
2020-10-21 13:51:38 +0800

    update minkowskisum

--- codebook/code/Geometry/minkowskisum.cpp
+++ codebook/code/Geometry/minkowskisum.cpp
@@ -1,26 +1,27 @@
-vector<Pt> minkowski(vector<Pt> p, vector<Pt> q){
+bool cmp(const pll &p,const pll &q){
+  int a=(p.x<0||(p.x==0&&p.y<0));
+  int b=(q.x<0||(q.x==0&&q.y<0));
+  if(a!=b)return a<b;
+  return (p^q)>0;
+}
+vector<pll> minkowski(vector<pll> p, vector<pll> q){
   int n = p.size() , m = q.size();
-  Pt c = Pt(0, 0);
-  for( int i = 0; i < m; i ++) c = c + q[i];
-  c = c / m;
-  for( int i = 0; i < m; i ++) q[i] = q[i] - c;
-  int cur = -1;
-  for( int i = 0; i < m; i ++)
-    if( (q[i] ^ (p[0] - p[n-1])) > -eps)
-      if( cur == -1 || (q[i] ^ (p[0] - p[n-1])) >
-                       (q[cur] ^ (p[0] - p[n-1])) )
-        cur = i;
-  vector<Pt> h;
-  p.push_back(p[0]);
-  for( int i = 0; i < n; i ++)
-    while( true ){
-      h.push_back(p[i] + q[cur]);
-      int nxt = (cur + 1 == m ? 0 : cur + 1);
-      if((q[cur] ^ (p[i+1] - p[i])) < -eps) cur = nxt;
-      else if( (q[nxt] ^ (p[i+1] - p[i])) >
-               (q[cur] ^ (p[i+1] - p[i])) ) cur = nxt;
-      else break;
-    }
-  for(auto &&i : h) i = i + c;
-  return convex_hull(h);
+  pll st=p[0]+q[0];
+  vector<pll> v1,v2,v(n + m);
+  for(int i = 0;i<n;i++) v1.pb(p[(i+1)%n]-p[i]);
+  for(int i = 0;i<m;i++) v2.pb(q[(i+1)%m]-q[i]);
+  merge(v1.begin(),v1.end(),v2.begin(),v2.end(),v.begin(),cmp);
+  vector<pll> h{st};
+  pll last=mp(0,0);
+  int fi=1;
+  for(auto it:v){
+    st=st+it;
+    if(!fi&&!cmp(last,it)&&!cmp(it,last))
+      h.pop_back();
+    last=it;
+    fi=0;
+    h.pb(st);
+  }
+  h.pop_back();
+  return h;
 }
